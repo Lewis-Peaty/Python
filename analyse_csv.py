1b64e82354dec821407cf1697523c6933a2248f0
Lewis-Peaty <lewis.peaty@gmail.com>
2016-01-29 20:13:32 +0800

    Update and rename analyseBigCSV.py to analyse_csv.py

--- analyse_csv.py
+++ analyse_csv.py
@@ -1,8 +1,11 @@
+# Syntax for reading data from a csv.
+# Originally used for processing some MW timeseries data.
+
 import csv
 with open('CGW TOTAL MEGAWATTS 3 YEARS.csv', 'rb') as csvfile:
-     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
      previousRowMW = 0
-     for row in spamreader:
+     for row in reader:
 #         print ', '.join(row)
          dPdt = float(row[1]) - previousRowMW
          if dPdt > 33.3 or dPdt < -33.3:
